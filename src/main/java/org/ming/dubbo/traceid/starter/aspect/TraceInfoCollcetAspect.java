a148273acd38753ff85a8ad27b58f8115285d95e
mingxiangjun <mingxiangjun@guazi.com>
2018-08-27 19:39:33 +0800

    完善逻辑

--- src/main/java/org/ming/dubbo/traceid/starter/aspect/TraceInfoCollcetAspect.java
+++ src/main/java/org/ming/dubbo/traceid/starter/aspect/TraceInfoCollcetAspect.java
@@ -3,7 +3,6 @@ package org.ming.dubbo.traceid.starter.aspect;
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.rpc.RpcContext;
 import lombok.extern.log4j.Log4j2;
-import org.apache.log4j.MDC;
 import org.apache.logging.log4j.ThreadContext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -11,6 +10,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.ming.dubbo.traceid.starter.exception.TraceInfoCollectException;
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -23,53 +23,76 @@ import java.util.UUID;
 @Log4j2
 public class TraceInfoCollcetAspect {
 
-    private static final String TRACE_ID_KEY = "traceId";
+    /**
+     * 全链路追踪key
+     */
+    private static final String TRACE_ID_KEY = "requestId";
     private static final String APP_NAME_KEY = "appName";
+    private static final String UNKNOWN_KEY = "unknown";
 
-    @Around(value = "execution(* *..*ServiceImpl.*(..))")
+    @Around(value = "execution(* *..*ServiceImpl.*(..)) or execution(* *..*Controller.*(..))")
     public Object collectTraceInfo(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        log.info("==============>>>>>collect rpc info<<<<<<=============");
-        // 获取traceId
-        Object traceId = MDC.get(TRACE_ID_KEY);
         RpcContext context = RpcContext.getContext();
 
+        // 获取traceId
+        Object traceId = getTraceId(context);
+
         if (context.isProviderSide()) {
-            log.info("==============>>>>>collect rpc info from provider>>>>>>>");
             // 服务提供方
-            if (StringUtils.isEmpty(traceId) && context.getAttachment(TRACE_ID_KEY) == null) {
-                traceId = UUID.randomUUID().toString().replaceAll("-", "");
-            } else {
-                traceId = context.getAttachment(TRACE_ID_KEY);
-            }
-            // 服务提供方，从attachment中获取调用方AppName
+            log.info("==============>>>>>collect rpc info from provider>>>>>>>");
+            // 从attachment中获取调用方AppName
             String appName = context.getAttachment(APP_NAME_KEY);
-            traceId = traceId.toString() + appName;
-            MDC.put(TRACE_ID_KEY, traceId);
+            appName = StringUtils.isEmpty(appName)?UNKNOWN_KEY:appName;
             ThreadContext.put(TRACE_ID_KEY, traceId.toString());
+            ThreadContext.put(APP_NAME_KEY,appName);
+            log.info("remote application:{} call local dubbo method {}",appName,context.getMethodName());
         } else if (context.isConsumerSide()) {
             log.info("<<<<<<collect rpc info from consumer<<<<<<<<=============");
-            // 如果traceId 不存在，则生成一个32位uuid
-            if (StringUtils.isEmpty(traceId)) {
-                traceId = UUID.randomUUID().toString().replaceAll("-", "");
-            }
             // 服务消费方
             if (!StringUtils.isEmpty(context.getAttachment(APP_NAME_KEY))) {
                 throw new TraceInfoCollectException("RpcContext Attachment get the same key：appName.you must define a new key in your application");
             }
             // 服务消费方，从url中获取本地appName，并放入rpcContext
             URL rpcUrl = context.getUrl();
-            String application = rpcUrl!=null?rpcUrl.getParameter("application"):"empty application";
+            String application = rpcUrl!=null?rpcUrl.getParameter("application"): UNKNOWN_KEY;
             context.setAttachment(APP_NAME_KEY, application);
             context.setAttachment(TRACE_ID_KEY, traceId.toString());
-            traceId = traceId.toString() + application;
-            MDC.put(TRACE_ID_KEY, traceId);
             ThreadContext.put(TRACE_ID_KEY, traceId.toString());
+            ThreadContext.put(APP_NAME_KEY,application);
+            log.info("call remote dubbo method {}",context.getMethodName());
+        } else {
+            // 找不到dubbo服务上下文
+            context.setAttachment(TRACE_ID_KEY,traceId.toString());
+            ThreadContext.put(TRACE_ID_KEY, traceId.toString());
+            log.info("call method {}",context.getMethodName());
         }
+
         // 正式调用方法
+        long startTime = System.currentTimeMillis();
+        log.info("className={}, methodName={}, params={}", proceedingJoinPoint.getSignature().getDeclaringTypeName(), proceedingJoinPoint.getSignature().getName(), Arrays.toString(proceedingJoinPoint.getArgs()));
         Object result = proceedingJoinPoint.proceed();
-        // 方法调用完成后清除MDC
-        MDC.clear();
+        log.info("耗时={}(ms), className={}, methodName={}, result={}", System.currentTimeMillis() - startTime, proceedingJoinPoint.getSignature().getDeclaringTypeName(), proceedingJoinPoint.getSignature().getName(), result);
+
+        // 方法调用完成后清除ThreadContext
         ThreadContext.clearAll();
         return result;
+    }
+
+    /**
+     * 获取traceId<br/>
+     *  如果traceId为空，则判断context中是否包含traeId：有则直接取，没有则生成
+     * @param context
+     * @return
+     */
+    private Object getTraceId(RpcContext context) {
+        Object traceId = ThreadContext.get(TRACE_ID_KEY);
+        if (StringUtils.isEmpty(traceId)) {
+            if (context.getAttachment(TRACE_ID_KEY) == null){
+                traceId = UUID.randomUUID().toString().replaceAll("-", "");
+            }else{
+                traceId = context.getAttachment(TRACE_ID_KEY);
+            }
+        }
+        return traceId;
     }
 }
